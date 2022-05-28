1e062c60b2bdd83c6ec873692b75b5e0c3b6b056
rodriguesnivea <106412675+rodriguesnivea@users.noreply.github.com>
2022-05-27 21:27:04 -0300

    alterando print dentro da classe main

--- src/curso_java_eclipse/Main.java
+++ src/curso_java_eclipse/Main.java
@@ -3,5 +3,6 @@ package curso_java_eclipse;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("esse eh meu primeiro repo.");
+		System.out.println("mas agora estou alterando do proprio github.");
 	}
 }

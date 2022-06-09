2bac1cd5c46bfb133334ee4a1efe188da48e38aa
rodriguesnivea <rodriguesnivea779@gmail.com>
2022-06-09 14:27:15 -0300

    alterando repositorio

--- src/curso_java_eclipse/Main.java
+++ src/curso_java_eclipse/Main.java
@@ -2,7 +2,6 @@ package curso_java_eclipse;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("esse eh meu primeiro repo.");
-		System.out.println("mas agora estou alterando do proprio github.");
+		System.out.println("alterando repositorio em Java");
 	}
 }
